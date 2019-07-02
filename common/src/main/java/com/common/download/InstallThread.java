ccfa40d3f5525cacb61b6136a4babbb3f2670d8b
LENOVO\lining21 <lining21@lenovo.com>
2019-07-02 18:24:21 +0800

    优化安装逻辑

--- common/src/main/java/com/common/download/InstallThread.java
+++ common/src/main/java/com/common/download/InstallThread.java
@@ -49,7 +49,7 @@ public class InstallThread extends Thread {
         }
         @Override
         public void onFinish() {
-            Log.i(Downer.TAG, "DownerUtil:isAppInstalled:packagename install status "+DownerUtil.isAppInstalled(mContext, downerRequest.apkPageName));
+            Log.i(Downer.TAG, "InstallThread:PauseTimer:"+downerRequest.apkPageName+" install status "+DownerUtil.isAppInstalled(mContext, downerRequest.apkPageName));
             pauseTimer.cancel();
         }
     };
